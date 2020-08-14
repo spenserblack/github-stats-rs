103c8b0c071fe0a089e7a686c85a0b32e3357cb2
Spenser Black <spenserblack01@gmail.com>
2020-08-14 15:39:34 -0400

    Add panicking example

---
+++ examples/search.rs
@@ -0,0 +1,34 @@
+use clap::{App, Arg};
+use github_stats::{Query, Search};
+
+#[tokio::main]
+async fn main() {
+    let matches = App::new("example")
+        .arg(
+            Arg::with_name("owner").index(1).required(true)
+        )
+        .arg(
+            Arg::with_name("repo").index(2).required(true)
+        )
+        .get_matches();
+
+    let owner = matches.value_of("owner").unwrap();
+    let repo = matches.value_of("repo").unwrap();
+
+    println!("Getting total merged PRs of {}/{}", owner, repo);
+
+    let query = Query::new()
+        .repo(owner, repo)
+        .is("pr")
+        .is("merged");
+
+    let results = Search::issues(&query)
+        .per_page(10)
+        .page(1)
+        .search()
+        .await
+        .unwrap();
+
+
+    println!("Total merged PRs: {}", results.total_count());
+}
